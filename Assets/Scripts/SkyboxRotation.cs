5fe1ea7498550620ad968282a86128bac976723d
Smashface <51160929+Smashface90@users.noreply.github.com>
2020-02-19 12:43:30 +0800

    Alpha Build 0.03:003

---
+++ Assets/Scripts/SkyboxRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotation : MonoBehaviour
+{
+    public float skiSpeedMultiplier;
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skiSpeedMultiplier);
+    }
+
+}
