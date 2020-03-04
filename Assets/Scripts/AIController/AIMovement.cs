dc628b6a29737fa33b8490b34cf00df4a55c2f65
ClockworkProduction <51160854+ClockworkProduction@users.noreply.github.com>
2020-03-04 17:19:46 +0800

    Alpha Build 0.05:001
    Basic AI scripting practice

---
+++ Assets/Scripts/AIController/AIMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    public GameObject Player;
+    public float MovementSpeed;
+    public float StoppingDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Player.transform.position) > StoppingDistance);
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, MovementSpeed * Time.deltaTime);
+
+        }
+
+       
+
+    }
+}
