3dddda2fb64b1852f30f58000379f18e71d15db7
ClockworkProduction <51160854+ClockworkProduction@users.noreply.github.com>
2020-03-12 13:59:56 +0800

    Alpha Build 0.06:003

--- Assets/Scripts/AIController/AIMovement.cs
+++ Assets/Scripts/AIController/AIMovement.cs
@@ -6,19 +6,26 @@ public class AIMovement : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Bullet;
+    public GameObject BulletEmitter;
+    public float BulletForwardForce;
     public float MovementSpeed;
     public float StoppingDistance;
     public Transform target;
+    public bool IsShooting;
+    public int BulletRate = 3;
+    public int Health;
 
     // Start is called before the first frame update
     void Start()
     {
+        // find the player
         Player = GameObject.FindWithTag("Player");  
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if withen a certain range, follow the player
         if (Vector3.Distance(transform.position, Player.transform.position) > StoppingDistance)
         {
 
@@ -26,14 +33,25 @@ public class AIMovement : MonoBehaviour
 
         }
 
-        transform.LookAt(Player.transform);
+        // When stopped, Shoot!
+        if (Vector3.Distance(transform.position, Player.transform.position) < StoppingDistance)
+        {
+            IsShooting = true;
+        }
 
-        //once stopped, shoot!
+            transform.LookAt(Player.transform);
 
+        // shooting
+        for (var i = 0; i < BulletRate; i++)
+        {
+            if (IsShooting == true)
+            {
+              //  BulletEmitter.transform.rotation=
+                Instantiate(Bullet, BulletEmitter.transform.position, BulletEmitter.transform.rotation);
+
+            }
+        }
         //once attack is finished, chase the player
-
-
-        // health
 
         // when he dies
     }
