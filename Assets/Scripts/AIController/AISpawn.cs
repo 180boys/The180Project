f7f684fea849230711eab0f792def6be1df56bbd
ClockworkProduction <51160854+ClockworkProduction@users.noreply.github.com>
2020-04-16 17:46:31 +0800

    0.11:012

---
+++ Assets/Scripts/AIController/AISpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AISpawn : MonoBehaviour
+{
+    public float timer = 30f;
+    public GameObject Enemy;
+    public Transform Spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", timer, timer);
+        Instantiate(Enemy, Spawn.position, Spawn.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            timer = 30f;
+        }
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(Enemy, Spawn.position, Spawn.rotation);
+    }
+}
